ccfe34272d2eaae56fe38d372882015433ddddc8
wdiasmaciel <wdiasmaciel@gmail.com>
2024-10-23 21:45:58 +0000

    Classe Fatorial.

--- Fatorial.java
+++ Fatorial.java
@@ -1,6 +1,9 @@
 public class Fatorial {
     // Método recursivo para calcular o fatorial:
     public double fat(int n) {
+        if (n < 0)
+            return 0;
+            
         // Caso base:
         if (n == 0 || n == 1) {
             return 1;
